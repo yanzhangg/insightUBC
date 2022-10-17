80ba39083b41a78afccb74232b77ddd52dcf5c58
ashyuan <ashleyblossomyuan@gmail.com>
2022-10-17 00:08:14 -0700

    C1-completed addDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -22,98 +22,65 @@ export default class InsightFacade implements IInsightFacade {
 	private dataset: any[];
 	private course: any[];
 	private allDatasetIds: string[];
+	private error: string;
+	private jsonKeys: string[];
+
 	constructor() {
 		console.log("InsightFacadeImpl::init()");
 		this.dataset = [];
 		this.course = [];
 		this.allDatasetIds = [];
+		this.error = "";
+		this.jsonKeys = ["Subject", "Course", "Avg", "Professor", "Title", "Pass", "Fail", "Audit", "id", "Year",
+			"Section"];
 	}
 
 										/** addDataset Methods **/
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		// Check for invalid dataset id
-		if (!this.checkValidDatasetID(id)) {
-			return Promise.reject(new InsightError("Invalid dataset ID"));
+		// Reset dataset array
+		this.dataset = [];
+		if (!this.isDatasetValid(id, content, kind)) {
+			return Promise.reject(new InsightError(`Invalid dataset ${this.error}`));
 		}
-		// **TODO: check if file is .zip
-		// if (content.substring(content.lastIndexOf(".") + 1) === "zip") { // !!! not working
-		// 	return Promise.reject(new InsightError("Dataset provided not a zip file"));
-		// }
-		// **TODO: unzip file using JSZip
+		// Unzip valid files
 		const jsZip = new JSZip();
-		const jsonKeys = ["Subject", "Course", "Avg", "Professor", "Title", "Pass", "Fail", "Audit", "id", "Year",
-			"Section"];
-
 		return jsZip.loadAsync(content, {base64: true}).then((zip) => {
 			const zipData: any[] = [];
-
-			zip.folder("courses")?.forEach((filename, zipObj) => {
+			if (!this.isZipValid(zip)) {
+				return Promise.reject(new InsightError(`Invalid dataset ${this.error}`));
+			}
+			zip.folder("courses")?.forEach((_, zipObj) => {
 				zipData.push(zipObj.async("text"));
 			});
-
-			// Reject if empty dataset
 			return (zipData.length === 0) ?
-				Promise.reject(new InsightError("Dataset empty")) : Promise.all(zipData.slice(1));
+				Promise.reject(new InsightError("Dataset empty 1")) : Promise.all(zipData.slice(1));
 		}).then((zipData: any[]) => {
 			zipData.forEach((data: any) => {
-				// **TODO: skip over invalid JSON files (check if it is in JSON)
+				this.course = [];
 				// Parse valid course files into a data structure
-				let jsonObject = JSON.parse(data);
-				jsonObject.result.forEach((sectionJSON: any) => {
-						// Check if each JSON section includes all query keys
-					if (jsonKeys.every((key) => Object.keys(sectionJSON).includes(key))) {
-							// let sectionObject = new SectionObject(
-							// 	id,
-							// 	String(sectionJSON["Subject"]),
-							// 	String(sectionJSON["Course"]),
-							// 	Number(sectionJSON["Avg"]),
-							// 	String(sectionJSON["Professor"]),
-							// 	String(sectionJSON["Title"]),
-							// 	Number(sectionJSON["Pass"]),
-							// 	Number(sectionJSON["Fail"]),
-							// 	Number(sectionJSON["Audit"]),
-							// 	String(sectionJSON["id"]),
-							// 	(sectionJSON["Section"] === "overall") ? 1900 : Number(sectionJSON["Year"]));
-
-							// Without using class SectionObject:
-						let sectionObject: {[key: string]: any} = {};
-						sectionObject[`${id}_dept`] = String(sectionJSON["Subject"]);
-						sectionObject[`${id}_id`] = String(sectionJSON["Course"]);
-						sectionObject[`${id}_avg`] = Number(sectionJSON["Avg"]);
-						sectionObject[`${id}_instructor`] = String(sectionJSON["Professor"]);
-						sectionObject[`${id}_title`] = String(sectionJSON["Title"]);
-						sectionObject[`${id}_pass`] = Number(sectionJSON["Pass"]);
-						sectionObject[`${id}_fail`] = Number(sectionJSON["Fail"]);
-						sectionObject[`${id}_audit`] = Number(sectionJSON["Audit"]);
-						sectionObject[`${id}_uuid`] = String(sectionJSON["id"]);
-						sectionObject[`${id}_year`] =
-								(sectionJSON["Section"] === "overall") ? 1900 : Number(sectionJSON["Year"]);
-
+				let jsonObject = this.parseValidJSON(data);
+				jsonObject?.result.forEach((sectionJSON: any) => {
+					// Check if each JSON section includes all query keys
+					if (this.jsonKeys.every((key) => Object.keys(sectionJSON).includes(key))) {
+						let sectionObject = this.createSectionObject(id, sectionJSON);
 						this.course.push(sectionObject);
 					}
 				});
+				if (this.course.length !== 0) {
+					this.dataset.push(this.course);
+				}
 			});
-			this.dataset.push(this.course);
+
+			if (this.dataset.length === 0) {
+				return Promise.reject(new InsightError("Dataset Empty"));
+			}
 			this.allDatasetIds.push(id);
 			this.saveFileToDisk(id);
-
 			return Promise.resolve(this.allDatasetIds);
 		}).catch((err) => {
 			return Promise.reject(new InsightError(err));
 		});
-
-			// **TODO: check if dataset is valid
-	}
-
-	// Helper function to check if dataset ID is valid
-	private checkValidDatasetID(id: string): boolean {
-		return !(id.includes("_") ||
-			   id.trim().length === 0 ||
-			   id === "" ||
-			   id === null ||
-			   id === undefined ||
-			   this.allDatasetIds.includes(id));
 	}
 
 	// Helper function to save file to disk (__dir/data)
@@ -126,14 +93,72 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	// Helper function to check if dataset is valid
-	// **TODO: has to contain at least one valid course section that meets the requirements below:
+	// has to contain at least one valid course section that meets the requirements below:
 	//         - root directory contains a folder called courses/ (CAUGHT IN CATCH ALL)
 	//         - valid courses will always be in JSON format
 	//         - each JSON file represents a course and can contain zero or more course valid sections
 	//         - a valid section must contain every field used which can be used by a query (DONE)
 	private isDatasetValid(id: string, content: string, kind: InsightDatasetKind): boolean {
-		// Check if courses are in JSON format
-		return false;
+		// Check for valid id
+		if (id.includes("_") || id.trim().length === 0 || id === "" || id === null || id === undefined ||
+			this.allDatasetIds.includes(id)) {
+			this.error = "id";
+			return false;
+		}
+		// check for Sections kind
+		if (kind !== InsightDatasetKind.Sections) {
+			this.error = "kind";
+			return false;
+		}
+		this.error = "";
+		return true;
+	}
+
+	// Helper function to check if zip file is valid
+	private isZipValid(zip: JSZip): boolean {
+		// check for courses folder
+		if (!Object.keys(zip.files).includes("courses/")) {
+			this.error = "no courses folder";
+			return false;
+		}
+		// check for at least one section in courses folder
+		let filenames: string[] = [];
+		zip.folder("courses")?.forEach((filename) => {
+			filenames.push(filename);
+		});
+		if (filenames.length === 0) {
+			this.error = "zip folder empty";
+			return false;
+		}
+		this.error = "";
+		return true;
+	}
+
+	// Helper function to parse valid JSON files and skip invalid ones
+	private parseValidJSON(data: any): any {
+		try {
+			let validJSON = JSON.parse(data);
+			return validJSON;
+		} catch (err) {
+			return;
+		}
+	}
+
+	// Helper function to create an object for each section from parsed JSON data
+	private createSectionObject(id: string, sectionJSON: any): {[key: string]: any} {
+		const sectionObject: {[key: string]: any} = {};
+		sectionObject[`${id}_dept`] = String(sectionJSON["Subject"]);
+		sectionObject[`${id}_id`] = String(sectionJSON["Course"]);
+		sectionObject[`${id}_avg`] = Number(sectionJSON["Avg"]);
+		sectionObject[`${id}_instructor`] = String(sectionJSON["Professor"]);
+		sectionObject[`${id}_title`] = String(sectionJSON["Title"]);
+		sectionObject[`${id}_pass`] = Number(sectionJSON["Pass"]);
+		sectionObject[`${id}_fail`] = Number(sectionJSON["Fail"]);
+		sectionObject[`${id}_audit`] = Number(sectionJSON["Audit"]);
+		sectionObject[`${id}_uuid`] = String(sectionJSON["id"]);
+		sectionObject[`${id}_year`] =
+			(sectionJSON["Section"] === "overall") ? 1900 : Number(sectionJSON["Year"]);
+		return sectionObject;
 	}
 
 	public removeDataset(id: string): Promise<string> {
