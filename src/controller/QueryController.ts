4a8370481aa248f3b30b8994fd84c04eebf846eb
Yan <yyzhang@student.ubc.ca>
2022-10-20 16:54:51 -0700

    Implement Not and Logic Comparison queries

--- src/controller/QueryController.ts
+++ src/controller/QueryController.ts
@@ -21,32 +21,72 @@ export function filterQuery(whereObject: object, id: string): Promise<InsightRes
 	const dataset = JSON.parse(rawDataset.toString());
 	const datasetOnly: object[][] = dataset.slice(0, -1);
 
-	if (Object.keys(whereObject).length === 0) {
-		return Promise.resolve(getAllData(datasetOnly));
-	}
+	let filteredArr: InsightResult[] = [];
+	datasetOnly.forEach((course: object[]) => {
+		Array.from(course).forEach((section: any) => {
+			filteredArr.push(section);
+		});
+	});
 
-	return recurse(whereObject, datasetOnly);
+	if (Object.keys(whereObject).length === 0) {
+		return Promise.resolve(filteredArr);
+	}
+	let filteredResult: InsightResult[] | InsightError = recurse(whereObject, filteredArr, id);
+	if (filteredResult instanceof InsightError) {
+		return Promise.reject(filteredResult);
+	}
+	return Promise.resolve(filteredResult);
 }
 
-export function recurse(obj: object, datasetArr: object[][]): Promise<InsightResult[]> {
-	const key = Object.keys(obj)[0];
-
-	if (key === "IS") {
-		if (!checkType(obj[key as keyof object])) {
-			return Promise.reject(new InsightError("Invalid key value type"));
+export function recurse(obj: object, datasetArr: InsightResult[], id: string): InsightResult[] | InsightError {
+	const whereKey = Object.keys(obj)[0];
+	const whereValue = obj[whereKey as keyof object];
+	if (whereKey === "IS") {
+		const errorStr = checkWhereValue(whereValue, id);
+		if (errorStr !== "") {
+			return new InsightError(errorStr);
 		}
-		return sComparison(datasetArr, obj[key as keyof object]);
-	} else if (key === "LT" || key === "GT" || key === "EQ") {
-		if (!checkType(obj[key as keyof object])) {
-			return Promise.reject(new InsightError("Invalid key value type"));
+		return sComparison(datasetArr, whereValue);
+	} else if (whereKey === "LT" || whereKey === "GT" || whereKey === "EQ") {
+		const errorStr = checkWhereValue(whereValue, id);
+		if (errorStr !== "") {
+			return new InsightError(errorStr);
 		}
-		return mComparison(datasetArr, key, obj[key as keyof object]);
+		return mComparison(datasetArr, whereKey, whereValue);
+	} else if (whereKey === "AND" || whereKey === "OR") {
+		if (Object.keys(whereValue).length === 0 || Object.values(whereValue).length === 0 ||
+			Object.values(whereValue)[0] === undefined ||
+			Object.keys(Object.values(whereValue)[0] as object).length === 0) {
+			return new InsightError("Empty AND/OR query");
+		}
+		let finalResult: InsightResult[] | InsightError = [];
+		let resultsArr: InsightResult[][] = [];
+		Array.from(whereValue).forEach((filterObj) => {
+			let filterObjResult: InsightResult[] | InsightError = recurse(filterObj as object, datasetArr, id);
+			if (filterObjResult instanceof InsightError) {
+				finalResult = filterObjResult;
+				return;
+			}
+			resultsArr.push(filterObjResult);
+		});
+		if (finalResult instanceof InsightError) {
+			return finalResult;
+		}
+		if (whereKey === "AND") {
+			finalResult = resultsArr.reduce((arr1, arr2) => arr1.filter((object) => arr2.includes(object)));
+		} else {
+			let finalOrArr: InsightResult[] = [];
+			resultsArr.forEach((resultArr) => {
+				finalOrArr.push(...resultArr);
+			});
+			finalResult = [...new Set(finalOrArr)];
+		}
+		return finalResult;
+	} else if (whereKey === "NOT") {
+		return notFilter(whereValue, datasetArr, id);
 	} else {
-		return Promise.reject(new InsightError("Invalid query filter"));
+		return new InsightError("Invalid query filter");
 	}
-	// } else if (key === "NOT") {
-	// 	// recurse()
-	// } else if (key === "AND" || key === "OR") {
 }
 
 export function checkType(queryObj: object): boolean {
@@ -64,42 +104,53 @@ export function checkType(queryObj: object): boolean {
 	return false;
 }
 
-export function sComparison(datasetArr: object[][], isObj: object): Promise<InsightResult[]> {
+export function checkWhereValue(whereValue: object, id: string): string {
+	if (!checkType(whereValue)) {
+		return "Invalid key value type";
+	}
+	if (Object.keys(whereValue)[0].split("_")[0] !== id) {
+		return "Invalid dataset id key";
+	}
+	return "";
+}
+
+export function sComparison(datasetArr: InsightResult[], isObj: object): InsightResult[] | InsightError {
 	const skey: string = Object.keys(isObj)[0];
 	// Check that inputString is valid (inside columns)
 	if (typeof Object.values(isObj)[0] !== "string") {
-		return Promise.reject(new InsightError("SComparison: input not a string"));
+		return new InsightError("SComparison: input not a string");
 	}
 	const inputString: string = Object.values(isObj)[0];
 	let filteredIsArr: InsightResult[] = [];
     // Check for wildcards
 	if (inputString.slice(0, 1) === "*" && inputString.slice(-1) === "*") {
 		if (inputString.slice(1, -1) === undefined || inputString.slice(1, -1).includes("*")) {
-			return Promise.reject(new InsightError("SComparison: input contains asterisk"));
+			return new InsightError("SComparison: input contains asterisk");
 		}
 		filteredIsArr = getFilteredArray(datasetArr, skey, inputString.slice(1, -1), "includes");
 	} else if (inputString.slice(0, 1) === "*") {
 		if (inputString.slice(1) === undefined || inputString.slice(1).includes("*")) {
-			return Promise.reject(new InsightError("SComparison: input contains asterisk"));
+			return new InsightError("SComparison: input contains asterisk");
 		}
 		filteredIsArr = getFilteredArray(datasetArr, skey, inputString.slice(1), "endsWith");
 	} else if (inputString.slice(-1) === "*") {
 		if (inputString.slice(0, -1) === undefined || inputString.slice(0, -1).includes("*")) {
-			return Promise.reject(new InsightError("SComparison: input contains asterisk"));
+			return new InsightError("SComparison: input contains asterisk");
 		}
 		filteredIsArr = getFilteredArray(datasetArr, skey, inputString.slice(0, -1), "startsWith");
 	} else {
 		if (inputString === undefined || inputString.includes("*")) {
-			return Promise.reject(new InsightError("SComparison: input contains asterisk"));
+			return new InsightError("SComparison: input contains asterisk");
 		}
 		filteredIsArr = getFilteredArray(datasetArr, skey, inputString, "equals");
 	}
-	return Promise.resolve(filteredIsArr);
+	return filteredIsArr;
 }
 
-export function mComparison(datasetArr: object[][], mComparator: string, isObj: object): Promise<InsightResult[]> {
+export function mComparison(datasetArr: InsightResult[], mComparator: string, isObj: object):
+InsightResult[] | InsightError {
 	if (typeof Object.values(isObj)[0] !== "number") {
-		return Promise.reject(new InsightError("MComparison: input not a number"));
+		return new InsightError("MComparison: input not a number");
 	}
 	const mkey: string = Object.keys(isObj)[0];
 	const number: number = Object.values(isObj)[0];
@@ -113,39 +164,25 @@ export function mComparison(datasetArr: object[][], mComparator: string, isObj: 
 	} else if (mComparator === "EQ") {
 		filteredMComparisonArr = getFilteredArray(datasetArr, mkey, number, "EQ");
 	}
-	return Promise.resolve(filteredMComparisonArr);
+	return filteredMComparisonArr;
 }
 
-export function getFilteredArray(datasetArr: object[][], key: string, input: string | number, operation: string):
+export function getFilteredArray(datasetArr: InsightResult[], key: string, input: string | number, operation: string):
 InsightResult[] {
 	let filteredArr: InsightResult[] = [];
 	if (typeof input === "string") {
-		datasetArr.forEach((course: object[]) => {
-			Array.from(course).forEach((section: any) => {
-				if (compareStringInput(section[key as keyof object], operation, input)) {
-					filteredArr.push(section);
-				}
-			});
+		Array.from(datasetArr).forEach((section: any) => {
+			if (compareStringInput(section[key as keyof object], operation, input)) {
+				filteredArr.push(section);
+			}
 		});
 	} else {
-		datasetArr.forEach((course: object[]) => {
-			Array.from(course).forEach((section: any) => {
-				if (compareNumberInput(section[key as keyof object], operation, input)) {
-					filteredArr.push(section);
-				}
-			});
+		Array.from(datasetArr).forEach((section: any) => {
+			if (compareNumberInput(section[key as keyof object], operation, input)) {
+				filteredArr.push(section);
+			}
 		});
 	}
-	return filteredArr;
-}
-
-export function getAllData(datasetArr: object[][]): InsightResult[] {
-	let filteredArr: InsightResult[] = [];
-	datasetArr.forEach((course: object[]) => {
-		Array.from(course).forEach((section: any) => {
-			filteredArr.push(section);
-		});
-	});
 	return filteredArr;
 }
 
@@ -171,6 +208,24 @@ export function compareNumberInput(sectionValue: number, operation: string, inpu
 	}
 }
 
+export function notFilter(whereValue: object, datasetArr: InsightResult[], id: string): InsightResult[] | InsightError {
+	if (Object.keys(whereValue).length === 0) {
+		return new InsightError("Empty NOT query");
+	}
+
+	if (Object.keys(Object.keys(whereValue)).length !== 1) {
+		return new InsightError("Invalid NOT Query: Excess keys");
+	}
+	let notFilterResult: InsightResult[];
+	let filterResult: InsightResult[] | InsightError;
+	filterResult = recurse(whereValue, datasetArr, id);
+	if (filterResult instanceof InsightError) {
+		return filterResult;
+	}
+	notFilterResult = datasetArr.filter((object) => !(filterResult as InsightResult[]).includes(object));
+	return notFilterResult;
+}
+
 // Outputs query in the format provided by the OPTIONS
 export function outputQuery(filterResult: InsightResult[], queryOptions: object):
 Promise<InsightResult[]> {
@@ -191,12 +246,13 @@ Promise<InsightResult[]> {
 	Array.from(filterResult).forEach((object) => {
 		let objAsArray = Object.entries(object);
 		let filteredObj = objAsArray.filter(([key, value]) => queryColumns.includes(key));
-		finalResult.push(Object.fromEntries(filteredObj));
+		if (filteredObj.length !== 0) {
+			finalResult.push(Object.fromEntries(filteredObj));
+		}
 	});
 
 	if (queryOrder !== "") {
 		finalResult.sort((objA, objB) => objA[queryOrder] > objB[queryOrder] ? 1 : -1);
 	}
-
 	return Promise.resolve(finalResult);
 }
